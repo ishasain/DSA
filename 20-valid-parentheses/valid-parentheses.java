118450967029e25ce8155be8edc665e025fccace
Isha Sain <ishasain2004@gmail.com>
2026-02-13 11:07:05 +0530

    Time: 5 ms (17.06%) | Memory: 43.2 MB (72.72%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,19 +1,35 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            if (ch == '{' || ch == '[' || ch == '(') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty())
-                    return false;
+        // for (char ch : s.toCharArray()) {
+        //     if (ch == '{' || ch == '[' || ch == '(') {
+        //         stack.push(ch);
+        //     } else {
+        //         if (stack.isEmpty())
+        //             return false;
             
-            char top = stack.pop();
-            if ((ch == ')' && top != '(' || ch == ']' && top != '[' || ch == '}' && top != '{')) {
-                return false;
+        //     char top = stack.pop();
+        //     if ((ch == ')' && top != '(' || ch == ']' && top != '[' || ch == '}' && top != '{')) {
+        //         return false;
+        //     }
+        //     }
+        // }
+        // return stack.isEmpty();
+        HashMap<Character,Character>map=new HashMap<>();
+          map.put('{','}');
+          map.put('[',']');
+          map.put('(',')');
+
+          for (char ch : s.toCharArray()){
+            if(map.containsKey(ch)){
+                stack.push(ch);
             }
+            else{
+                if(stack.isEmpty()||map.get(stack.pop()) != ch){
+                    return false;
+                }
             }
-        }
-        return stack.isEmpty();
-    }
+          }
+          return stack.isEmpty();
+            }
 }

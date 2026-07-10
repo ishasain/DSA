6ac8b815d7d9e9f53dff7c6ab78f64dc3440517b
Isha Sain <ishasain2004@gmail.com>
2026-07-10 13:02:18 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (21.18%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,36 +1,19 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
-        }else if ( nums[0]!=nums[1]){
-            return nums[0];
-        }else if ( nums[nums.length-1] != nums[nums.length-2]){
-            return nums[nums.length-1];
-        }
-
-        int start = 0 ;
-        int end = nums.length-1 ;
-
-        while(start<=end){
-            int mid = (start + end)/2;
-
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }else if ( mid % 2 ==1 ){ 
-                if(nums[mid-1]==nums[mid]){
-                    start = mid+1 ;
-                }else{
-                    end = mid-1 ;
-                }
-            }else{
-                    if(nums[mid]==nums[mid+1]){
-                        start = mid +1 ;
-                    }else{
-                        end = mid -1 ;
-                    }
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+           int mid=(start+end)/2;
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]==nums[mid+1]){
+                start=mid+2;
+            }
+            else{
+                end=mid;
             }
         }
-
-        return -1 ;
+        return nums[start];
     }
 }

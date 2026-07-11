870baacc98b0e49cf23c54323b161fad77022afc
Isha Sain <ishasain2004@gmail.com>
2026-07-11 15:18:40 +0530

    Time: 1 ms (97.64%) | Memory: 42.5 MB (61.35%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+       if(x==0||x==1){
+        return x;
+       } 
+       int start=1;
+       int end=x;
+       int mid=-1;
+       int ans=1;
+       while(start<=end){
+        mid=start+(end-start)/2;
+        if((long)mid*mid<=x)
+        {
+            ans=mid;
+            start=mid+1;
+        }
+        else {
+            end=mid-1;
+        }
+       }
+       return end;
+    }
+}
